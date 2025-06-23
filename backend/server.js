c7dc1c5ac4f41ae001ff7dd99a70ec8080ba7a81
sushant <bhattaraisushant99@gmail.com>
2025-06-23 12:07:32 +0545

    added more imports

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from 'express';
+import doteenv from 'dotenv';
+import connectDB from './db.js';
 
 const app = express();
+doteenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello Worlddd!");
 });
 
 app.listen(3000, () => {
+  connectDB();
   console.log('Server is running on port 3000');
 });
