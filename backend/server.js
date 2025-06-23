4457369187aac536f05b062338dbfdc0f74abdc4
sushant <bhattaraisushant99@gmail.com>
2025-06-23 12:03:29 +0545

    added database

--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,3 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
-//HKPCFY25a2cVaodv
-//mongodb+srv://bhattaraisushant99:HKPCFY25a2cVaodv@cluster0.or2glpe.mongodb.net/
